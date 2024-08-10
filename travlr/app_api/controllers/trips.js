f79869d746ec7312f984d3f11f5937f4b322bb89
Marissa Sihapanya <marissasihapanya@gmail.com>
2024-08-10 18:30:49 -0500

    Module 6 completed baseline

--- travlr/app_api/controllers/trips.js
+++ travlr/app_api/controllers/trips.js
@@ -12,6 +12,39 @@ const tripsList = async (req, res) => {
     return res.status(200).json(q);
   }
 };
+// POST: /trips – Adds a new Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsAddTrip = async (req, res) => {
+  const newTrip = new Trip({
+    code: req.body.code,
+    name: req.body.name,
+    length: req.body.length,
+    start: req.body.start,
+    resort: req.body.resort,
+    perPerson: req.body.perPerson,
+    image: req.body.image,
+    description: req.body.description
+  });
+
+  const q = await newTrip.save();
+
+  if (!q) {
+    // Database returned no data
+    return res
+      .status(400)
+      .json(err);
+  } else {
+    // Return new trip
+    return res
+      .status(201)
+      .json(q);
+  }
+
+  // Uncomment the following line to show results of operation
+  // on the console
+  // console.log(q);
+};
 
 // GET: /trips/:tripCode - find trip by code
 const tripsFindByCode = async (req, res) => {
@@ -24,7 +57,51 @@ const tripsFindByCode = async (req, res) => {
   }
 };
 
+// PUT: /trips/:tripCode - Updates an existing Trip
+// Regardless of outcome, response must include HTML status code
+// and JSON message to the requesting client
+const tripsUpdateTrip = async (req, res) => {
+  // Uncomment for debugging
+  console.log(req.params);
+  console.log(req.body);
+
+  const q = await Model
+    .findOneAndUpdate(
+      { 'code': req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description
+      }
+    )
+    .exec();
+
+  if (!q) {
+    // Database returned no data
+    return res
+      .status(400)
+      .json(err);
+  } else {
+    // Return resulting updated trip
+    return res
+      .status(201)
+      .json(q);
+  }
+
+  // Uncomment the following line to show results of operation
+  // on the console
+  // console.log(q);
+};
+
+
 module.exports = {
   tripsList,
+  tripsAddTrip,
+  tripsUpdateTrip,
   tripsFindByCode
 };
